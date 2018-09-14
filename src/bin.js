062bbf773d48e222774c5da0de76e408ed8093cb
Charlie Jackson <charlie@charliejackson.com>
2018-09-14 21:53:58 +0100

    feat(Commitizen): Add commitizen for easily handling commits
    Prior to this change, there was no easy way to template commit messages
    
    BREAKING CHANGE: Everything has gone wrong

--- src/bin.js
+++ src/bin.js
@@ -2,4 +2,42 @@
 
 // @flow
 
+import editor from 'tiny-cli-editor';
+
 console.log('Commityy');
+
+editor('Hello this is dog.')
+  .on('data', (text) => {
+    console.log('data', text);
+  })
+  .on('abort', (text) => {
+    console.log('abort', text);
+
+    setTimeout(() => {
+      console.log('yay');
+    }, 3000);
+  })
+  .on('submit', (text) => {
+    console.log('submit', text);
+  });
+/*
+# If applied, this commit will... (limit)
+
+# Why is this change needed?
+Prior to this change, ...
+
+# How does it address the issue?
+This change...
+
+References
+- Jira: #GEN-254 - https://www....
+- Key: Value
+
+
+Title
+Why
+How
+Refs
+
+Originally commited on - Branch
+*/
