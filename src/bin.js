09d7e35c843908b59c370ea9f4946526a272929c
Charlie Jackson <charlie@charliejackson.com>
2018-09-15 12:23:57 +0100

    Did most of the message logic

--- src/bin.js
+++ src/bin.js
@@ -2,24 +2,188 @@
 
 // @flow
 
-import editor from 'tiny-cli-editor';
+import inquirer from 'inquirer';
+import emojic from 'emojic';
+import MaxLengthInputPrompt from 'inquirer-maxlength-input-prompt';
+import AutoComplete from 'inquirer-autocomplete-prompt';
+import fuzzy from 'fuzzy';
 
-console.log('Commityy');
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+inquirer.registerPrompt('autocomplete', AutoComplete);
 
-editor('Hello this is dog.')
-  .on('data', (text) => {
-    console.log('data', text);
-  })
-  .on('abort', (text) => {
-    console.log('abort', text);
+// http://emoji.muan.co/
 
-    setTimeout(() => {
-      console.log('yay');
-    }, 3000);
-  })
-  .on('submit', (text) => {
-    console.log('submit', text);
+const trim = (text) => {
+  if (text) return text.trim();
+
+  return text;
+};
+
+const getDetails = () =>
+  Promise.resolve({
+    branch: 'feature/GEN-264-somethind-and-another',
+    origin: 'https://github.com.git',
+    prevResponses: {
+      choices: ['bug', 'feat', 'feat'],
+      scope: ['withHOC', null, 'NewsCards'],
+    },
   });
+
+getDetails().then(({ prevResponses }) => {
+  const descriptionDefault =
+    "# Don't save changes to prevent this file getting adding\n# Why is this change needed?\nPrior to this change, \n\n# How does it address the issue?\nThis change";
+
+  let scopeSuggestions = [];
+
+  if (prevResponses.scope && prevResponses.scope.length) {
+    scopeSuggestions = prevResponses.scope.filter(val => !!val).reverse();
+  }
+
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: "Select the type of change that you're committing",
+        choices: [
+          'none',
+          'feat (A new feature)',
+          'fix (A bug fix)',
+          'docs (Documentation changes only)',
+          'style (Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc))',
+          'refactor (A code change that neither fixes a bug nor adds a feature)',
+          'perf (A code change that improves performance)',
+          'test (Adding missing tests or correcting existing tests)',
+          'chore (Updating build tasks etc; no production code change)',
+        ],
+        filter: answer => answer.split(' ')[0],
+      },
+      {
+        type: 'autocomplete',
+        name: 'scope',
+        suggestOnly: true,
+        source: (answers, input) =>
+          Promise.resolve(fuzzy
+            .filter(input || '', scopeSuggestions)
+            .map(({ original }) => original)),
+        message:
+          'What is the scope of this change (e.g. component or file name)?\n-',
+        filter: trim,
+      },
+    ])
+    .then(({ type, scope }) => {
+      let firstLine = '';
+
+      if (type !== 'none') {
+        firstLine = type;
+      }
+
+      if (scope) {
+        firstLine = `${firstLine}(${scope})`;
+      }
+
+      if (firstLine !== '') {
+        firstLine = `${firstLine}:`;
+      }
+
+      const maxFirstLineLength = 100;
+      const emojiAllowance = 2;
+      const lengthLeftForTitle =
+        maxFirstLineLength - emojiAllowance - firstLine.length;
+
+      return inquirer
+        .prompt([
+          {
+            type: 'maxlength-input',
+            name: 'title',
+            message: 'If applied, this commit will...\n-',
+            maxLength: lengthLeftForTitle,
+            validate: (text) => {
+              const filteredText = trim(text);
+
+              if (!filteredText || filteredText === '') {
+                return 'This is the commit title, it cannot be blank';
+              }
+
+              if (filteredText.length > lengthLeftForTitle) {
+                return `Title length cannot be longer than ${lengthLeftForTitle}. Currently at ${
+                  filteredText.length
+                }`;
+              }
+
+              return true;
+            },
+            filter: trim,
+          },
+          {
+            type: 'editor',
+            name: 'body',
+            message: 'Add a description to this commit',
+            default: descriptionDefault,
+            filter: (text) => {
+              if (text === descriptionDefault) return null;
+
+              let final = '';
+              let first = true;
+
+              text.split('\n').forEach((line) => {
+                if (line.startsWith('#')) return;
+
+                if (first) {
+                  first = false;
+                  final = line;
+                } else {
+                  final = `${final}\n${line}`;
+                }
+              });
+
+              return final;
+            },
+          },
+        ])
+        .then(({ title, body }) => {
+          firstLine = `${firstLine}${title}`;
+          // Refs - Loop over - remember prev jira issue?
+          const getRefs = () => Promise.resolve([]);
+
+          return getRefs().then(refs =>
+            inquirer
+              .prompt([
+                {
+                  type: 'list',
+
+                  name: 'emoji',
+                  choices: [
+                    'none',
+                    `${
+                      emojic.art
+                    } when improving the format/structure of the code`,
+                    `${emojic.racehorse} when improving performance`,
+                  ],
+                  filter: (text) => {
+                    if (!text || text === 'none') return null;
+
+                    return text.split(' ')[0];
+                  },
+                },
+              ])
+              .then(({ emoji }) => {
+                let gitMessage = `${firstLine}\n\n`;
+
+                if (emoji) {
+                  gitMessage = `${emoji} ${gitMessage}`;
+                }
+
+                if (body) {
+                  gitMessage = `${gitMessage}${body}\n\n`;
+                }
+
+                console.log(gitMessage);
+              }));
+        });
+    });
+});
+
 /*
 # If applied, this commit will... (limit)
 
@@ -40,4 +204,44 @@ How
 Refs
 
 Originally commited on - Branch
+
+# <type>(<scope>): <subject>
+#
+# <body>
+#
+# <footer>
+#
+# ** TYPES **
+# feat (new feature)
+# fix (bug fix)
+# docs (changes to documentation)
+# style (formatting, missing semi colons, etc; no code change)
+# refactor (refactoring production code)
+# test (adding missing tests, refactoring tests; no production code change)
+# chore (updating grunt tasks etc; no production code change)
+#
+# ** FOOTERS **
+# References #1, #4, and #2.
+# Fix #1. note this marks the item as accepted in Sprintly
+# Closes #1 and #2. note this marks the item as accepted in Sprintly
+
+# ** Funtip **
+# Work hard, play hard!  Consider prefixing your commit messages with a relevant emoji for
+# great good:
+#
+#   :art: `:art:` when improving the format/structure of the code
+#   :racehorse: `:racehorse:` when improving performance
+#   :non-potable_water: `:non-potable_water:` when plugging memory leaks
+#   :memo: `:memo:` when writing docs
+#   :penguin: `:penguin:` when fixing something on Linux
+#   :apple: `:apple:` when fixing something on Mac OS
+#   :checkered_flag: `:checkered_flag:` when fixing something on Windows
+#   :bug: `:bug:` when fixing a bug
+#   :fire: `:fire:` when removing code or files
+#   :green_heart: `:green_heart:` when fixing the CI build
+#   :white_check_mark: `:white_check_mark:` when adding tests
+#   :lock: `:lock:` when dealing with security
+#   :arrow_up: `:arrow_up:` when upgrading dependencies
+#   :arrow_down: `:arrow_down:` when downgrading dependencies
+#   :shirt: `:shirt:` when removing linter warnings
 */
