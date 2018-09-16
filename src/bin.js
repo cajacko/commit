5512845471fcfcebcecc8dd944d28bdc5384e4a1
Charlie Jackson <charlie@charliejackson.com>
2018-09-16 15:32:26 +0100

    "Test commit"

--- src/bin.js
+++ src/bin.js
@@ -3,6 +3,7 @@
 // @flow
 
 import inquirer from 'inquirer';
+import { runCommand } from '@cajacko/template';
 import MaxLengthInputPrompt from 'inquirer-maxlength-input-prompt';
 import AutoComplete from 'inquirer-autocomplete-prompt';
 import getMessage from './message/getMessage';
@@ -10,6 +11,15 @@ import getMessage from './message/getMessage';
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 inquirer.registerPrompt('autocomplete', AutoComplete);
 
-getMessage().then((message) => {
-  console.log(message);
-});
+// TODO: If commit failed, store message and ask if want to use it
+// TODO: getMessage
+// TODO: saveResponses for getMessage, when done
+// TODO: ask for message and then commit with the message
+
+runCommand('git commit -m $1', { vars: { $1: 'Test commit' } });
+
+// getMessage().then((message) => {
+//   console.log(message);
+
+//   runCommand('ls');
+// });
