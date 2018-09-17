285a2d3b4867b73371a875b7705d0cb8b7121c0b
Charlie Jackson <charlie@charliejackson.com>
2018-09-17 19:37:40 +0100

    feat: Stage all changes if nothing else is staged
    References:
    - Original Branch: master

--- src/bin.js
+++ src/bin.js
@@ -13,29 +13,43 @@ import { set, remove } from './utils/store';
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 inquirer.registerPrompt('autocomplete', AutoComplete);
 
+// TODO: Final confirm is Yne - e is editor, same it confirming previous commit
 // TODO: Can pass flag to do a short commit
 // TODO: Ask if we to do description and open editor right after if true
-// TODO: If no staged ask to stage all
+// TODO: Reorder all choices based on usage (last used is high, frequently used is high)
+// TODO: "Related to" ordered above "fixes"
+// TODO: Ability to force all commits to run through us
+// TODO: Can enter cutom emoji
 
 git.hasStagedChanges(process.cwd()).then((hasStagedChanges) => {
-  if (!hasStagedChanges) {
-    throw new Error('Nothing is staged');
-  }
+  /**
+   *
+   */
+  const commit = () =>
+    getDetails().then(details =>
+      getMessage(details).then((message) => {
+        if (!message) return Promise.resolve();
 
-  return getDetails().then(details =>
-    getMessage(details).then((message) => {
-      if (!message) return Promise.resolve();
+        const { storeKey } = details;
 
-      const { storeKey } = details;
+        return git
+          .commit(process.cwd(), message, false)
+          .then(() => {
+            remove([storeKey, 'failedMessage']);
+          })
+          .catch((e) => {
+            set([storeKey, 'failedMessage'], message);
+            throw e;
+          });
+      }));
 
-      return git
-        .commit(process.cwd(), message, false)
-        .then(() => {
-          remove([storeKey, 'failedMessage']);
-        })
-        .catch((e) => {
-          set([storeKey, 'failedMessage'], message);
-          throw e;
-        });
-    }));
+  if (hasStagedChanges) return commit();
+
+  return git.hasUnstagedChanges(process.cwd()).then((hasUnstagedChanges) => {
+    if (!hasUnstagedChanges) {
+      throw new Error('There are no changes to commit');
+    }
+
+    return git.stageAll().then(commit);
+  });
 });
