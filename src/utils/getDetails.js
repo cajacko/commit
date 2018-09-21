90b6b4d74c955978709d7404f745339221ca1b04
Charlie Jackson <charlie@charliejackson.com>
2018-09-21 09:27:43 +0100

    🔧 refactor(storeKey): Split get store key into its own function
    - Original Branch: master

--- src/utils/getDetails.js
+++ src/utils/getDetails.js
@@ -3,6 +3,7 @@
 import { git } from '@cajacko/template';
 import lodashGet from 'lodash/get';
 import { get } from './store';
+import getStoreKey from './getStoreKey';
 
 /**
  * Get any config details from previous responses related to this repo and
@@ -13,12 +14,11 @@ import { get } from './store';
 const getDetails = () =>
   git.getRootDir(process.cwd()).then(rootDir =>
     Promise.all([
+      getStoreKey(),
       git.getOrigin().catch(() => null),
       git.getCurrentBranch(),
-    ]).then(([origin, branch]) => {
-      const storeKey = origin || rootDir;
-
-      return get().then((storeSettings) => {
+    ]).then(([storeKey, origin, branch]) =>
+      get().then((storeSettings) => {
         const settings = storeSettings || {};
 
         return {
@@ -37,7 +37,6 @@ const getDetails = () =>
           lastUsedTags: lodashGet(settings, [storeKey, 'lastUsedTags']) || [],
           failedMessage: lodashGet(settings, [storeKey, 'failedMessage'], null),
         };
-      });
-    }));
+      })));
 
 export default getDetails;
